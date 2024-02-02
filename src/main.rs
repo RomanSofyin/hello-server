e602fc5d5a73356e1c5112e320f4f806dfdf1f5c
Roman Sofin <r.sofin@yadro.com>
2024-02-02 18:07:57 +0300

    Keep implementing hello-server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hello_server::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufRead, BufReader},
@@ -6,12 +7,14 @@ use std::{
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    // let listener = TcpListener::bind("10.0.2.15:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        })
     }
 }
 
@@ -19,21 +22,15 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("./src/hello.html").unwrap();
-        let length = contents.len();
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");    
-        stream.write_all(response.as_bytes()).unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "./src/hello.html")
     } else {
-        // todo!();
-    }
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
+        ("HTTP/1.1 404 NOT FOUND", "./src/404.html")
+    };
 
-    // println!("Request: {:#?}", http_request);
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");    
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
