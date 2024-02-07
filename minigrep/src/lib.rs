ee0f938540023d81e3280d68bc4097c3ef1a90d9
Roman Sofin <r.sofin@yadro.com>
2024-02-07 09:35:48 +0300

    Add tests mod and search fn

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, result};
 
 pub struct Config {
     // ToDo: `query` and `file_path` needs become private later
@@ -31,11 +31,37 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents =
-        fs::read_to_string(config.file_path)?;
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
